4004a8c88ed847e5b436b42fb982047ccbd0fd8f
abigailaiyun <115044972+abigailaiyun@users.noreply.github.com>
2023-03-22 17:06:42 -0700

    Update courses.py

--- courses.py
+++ courses.py
@@ -1,12 +1,9 @@
-from uwtools import course_catalogs
-import json
 import uwtools
 
-# Get the course catalogs from the uwtools library as a
-# python Dictionary
-course_catalog_dict = uwtools.course_catalogs(struct='dict')
+#PROB: cannot find uwtools (hints that it is not installed but it is)
 
-with open('courses.json', mode='w') as f:
-    # Indent and sort_keys are optional parameters, but help when looking 
-    # through the .json file
-    json.dump(course_catalog_dict, f, indent=4, sort_keys=True)
+# Get the course catalogs from the uwtools library as a
+# pandas DataFrame
+course_catalog_df = uwtools.course_catalogs(struct='df')
+
+# course_catalog_df.to_csv('FILE_LOCATION')
