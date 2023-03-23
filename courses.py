d32757e71e9797d244760a7209718881fda68380
abigailaiyun <115044972+abigailaiyun@users.noreply.github.com>
2023-03-23 12:51:39 -0700

    update python and html file tests

--- courses.py
+++ courses.py
@@ -1,12 +1,12 @@
 import json
-import uwtools
+# import uwtools
 from uwtools import course_catalogs
 
 # Get the course catalogs from the uwtools library as a
 # python Dictionary
 course_catalog_dict = uwtools.course_catalogs(struct='dict')
 
-with open('parse1.json', mode='w') as f:
+with open('parse.json', mode='w') as f:
     # Indent and sort_keys are optional parameters, but help when looking 
     # through the .json file
     json.dump(course_catalog_dict, f, indent=4, sort_keys=True)
