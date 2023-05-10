01a6e61d8257b3b4b41ff8468aa4844bdc24ba0c
abigailaiyun <115044972+abigailaiyun@users.noreply.github.com>
2023-05-09 20:12:59 -0700

    package install

--- save.js
+++ save.js
@@ -23,83 +23,91 @@
 //     document.querySelector('form').reset();
 // }
 
-// let infoButton = document.getElementById("submitIt");
-// // // runs this code when the btnsub
-// if (infoButton) {
-//     infoButton.addEventListener('click', saveInfo, false);
-//     console.log("button");
-//   }
+// code needs to be in node.js, needs node manager
+// pluralist.com
 
-const form = document.querySelector('submitIt')
-if (form){
-form.addEventListener('submit', (e) => {
-    // stops the actual website from reloading
-    e.preventDefault();
-    const fs = require('fs');
-    const user = {
-        "name": userName.value,
-        "email": email.value,
-        "feedback": feedback.value,
-        "rating": 5
-    };
-    const data = JSON.stringify(user);
-    fs.writeFile('feedback.json', data, (err) => {
-        if (err) {
-            throw err;
+import { writeFile } from 'fs';
+
+window.addEventListener("load", () => {
+    const infoButton = document.getElementById("submitIt");
+    // // // runs this code when the btnsub
+    if (infoButton) {
+        infoButton.addEventListener('click', saveInfo);
+        console.log("An event has been added to infoButton");
+    }
+
+    const form = document.querySelector('submitIt')
+
+    if (form){
+    form.addEventListener('submit', (e) => {
+        // stops the actual website from reloading
+        e.preventDefault();
+        const fs = require('fs');
+        const user = {
+            "name": userName.value,
+            "email": email.value,
+            "feedback": feedback.value,
+            "rating": 5
+        };
+        const data = JSON.stringify(user);
+        fs.writeFile('feedback.json', data, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON data is saved.");
+        });
+        window.location.href = "";
+    })
+    }
+
+    function saveInfo(){
+        console.log("Feedback form has submitted");
+
+    // //     // if (rate1.value != null) {
+    // //     //     rate = 1;
+    // //     // }
+
+    // //     // if (rate2.value != null) {
+    // //     //     rate = 2;
+    // //     // }
+
+    // //     // if (rate3.value != null) {
+    // //     //     rate = 3;
+    // //     // }
+
+    // //     // if (rate4.value != null) {
+    // //     //     rate = 4;
+    // //     // }
+
+    // //     // if (rate5.value != null) {
+    // //     //     rate = 5;
+    // //     // }
+
+    //     // create a JSON object
+        const user = {
+            "name": userName.value,
+            "email": email.value,
+            "feedback": feedback.value,
+            "rating": 5
+        };
+
+        const finished = (error) => {
+            if(error){
+                console.error(error)
+                return;
+            }
         }
-        console.log("JSON data is saved.");
-    });
-    window.location.href = "";
+
+        // convert JSON object to string
+        const data = JSON.stringify(user);
+        writeFile('feedback.json', data, finished)
+        
+        // write JSON string to a file
+        writeFile('feedback.json', data, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("JSON data is saved.");
+        });
+    }
 })
-}
-
-// function saveInfo(){
-//     const fs = require('fs');
-
-// //     // if (rate1.value != null) {
-// //     //     rate = 1;
-// //     // }
-
-// //     // if (rate2.value != null) {
-// //     //     rate = 2;
-// //     // }
-
-// //     // if (rate3.value != null) {
-// //     //     rate = 3;
-// //     // }
-
-// //     // if (rate4.value != null) {
-// //     //     rate = 4;
-// //     // }
-
-// //     // if (rate5.value != null) {
-// //     //     rate = 5;
-// //     // }
-
-//     // create a JSON object
-//     const user = {
-//         "name": userName.value,
-//         "email": email.value,
-//         "feedback": feedback.value,
-//         "rating": 5
-//     };
-
-//     const finished = (error) => {
-//         if(error){
-//             console.error(error)
-//             return;
-//         }
-//     }
-
-//     // convert JSON object to string
-//     const data = JSON.stringify(user);
-//     fs.writeFile('feedback.json', data, finished)
-    
-//     // write JSON string to a file
-//     // fs.writeFile('feedback.json', data, (err) => {
-//     //     if (err) {
-//     //         throw err;
-//     //     }
-//     //     console.log("JSON data is saved.");
-//     // });
-// }
